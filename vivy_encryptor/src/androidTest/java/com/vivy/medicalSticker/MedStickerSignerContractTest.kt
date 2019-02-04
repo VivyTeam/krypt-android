c50bc08d6534aec1050fe0f14b38ee2ae6e644e3
Mohamed Elawadi <eng.mohammed.alawady@gmail.com>
2019-02-04 16:00:39 +0100

    [MAIN] Updating signature contract test

--- vivy_encryptor/src/androidTest/java/com/vivy/medicalSticker/MedStickerSignerContractTest.kt
+++ vivy_encryptor/src/androidTest/java/com/vivy/medicalSticker/MedStickerSignerContractTest.kt
@@ -9,27 +9,27 @@ class MedStickerSignerContractTest {
     val encrypt = MedStickerEncryption
     @Test
     fun signTestAdam() {
-        val pin = "yzuygF6M"
-        val code = "yeeXCYff"
+        val pin = "qmHuG263"
+        val code = "7i6XA2zz"
         val salt = "98C1EB4EE93476743763878FCB96A25FBC9A175074D64004779ECB5242F645E6"
         val att=encrypt.deriveKey(code,pin,MedStickerCipherAttr.ADAM)
 
         Assertions.assertThat(
             service.accessSignature(att,salt.toByteArray())
-        ) .isEqualTo("sha256wi5J6zNjkH2clai0Ygfh375AJbWcOURR4HJlIxht2wo=")
+        ) .isEqualTo("sha2566VMS5+x3CdtLE8gTzcGHcB29u6phH5IBEY7DKDHZv7w=")
     }
 
     @Test
     fun signTestBritney() {
-        val pin = "yzuygF6M"
-        val code = "yeeXCYff"
+        val pin = "qmHuG263"
+        val code = "7i6XA2zz"
         val salt = "98C1EB4EE93476743763878FCB96A25FBC9A175074D64004779ECB5242F645E6"
         val att=encrypt.deriveKey(code,pin,MedStickerCipherAttr.BRITNEY)
 
         Assertions.assertThat(
             service.accessSignature(att,salt.toByteArray())
         )
-            .isEqualTo("sha256FkZmKpxsOivCB1MzfDNq2J8EAfzEhyapfn3gpQfuDgY=")
+            .isEqualTo("sha256dmkV0L/hUFWkEEz0DnjvCCKfGAfLkfYv8ZaVuKZFP1w=")
     }
 
 }
